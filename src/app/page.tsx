0855d3d78ee34c7a64cab94a199c5da60dd381dc
Dmitry <dmitry.oborsky@gmail.com>
2025-07-14 14:48:39 +0300

    :sparkles: Added Button + IconButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Button } from '@/components/ui/Button';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import { IconButton } from '@/components/ui/IconButton';
 
 export default function Home() {
   return (
@@ -6,6 +10,19 @@ export default function Home() {
       <Typography variant="h3" color="textPrimary">
         Shoes Shop - Team 3
       </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          gap: '10px',
+        }}
+      >
+        <Button variant="contained" color="primary" size="small">
+          Click me
+        </Button>
+        <IconButton sx={{ backgroundColor: 'grey.200' }}>
+          <AccessAlarmIcon />
+        </IconButton>
+      </Box>
     </>
   );
 }
