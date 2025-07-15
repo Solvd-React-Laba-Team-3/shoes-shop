7bbd128d54c1719956de6aabea884b9e0105d96f
Evgenij <e.ilyin.work@gmail.com>
2025-07-15 17:36:33 +0200

    update: remove export

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,3 @@
 export { Button } from './Button/Button';
 export { IconButton } from './IconButton/IconButton';
 export { Checkbox } from './Checkbox/Checkbox';
-export { Accordion } from './Accordion/Accordion';
