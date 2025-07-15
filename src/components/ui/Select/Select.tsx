d6adfc83129032ef7226f7a254aa0db715e18bb6
olha36 <olhakucheruk36@gmail.com>
2025-07-15 22:41:08 +0300

    fix select by exhcanging prop into ...props and changing sx into styled component

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -1,61 +1,74 @@
 'use client';
-import { FC } from 'react';
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Select, { SelectProps } from '@mui/material/Select';
+import { FC } from 'react';
+import { styled } from '@mui/material/styles';
 
-type BasicSelectProps = {
+interface BasicSelectProps extends Omit<SelectProps, 'value'> {
   value: string;
   label?: string;
-  onChange?: (event: SelectChangeEvent) => void;
-  id?: string;
-  name?: string;
-};
+  error?: boolean;
+}
+
+const StyledSelect = styled(Select)(({ theme }) => ({
+  backgroundColor: 'white',
+  fontSize: 10,
+  padding: 2,
+  color: theme.palette.text.secondary,
+  '& .MuiSelect-select': {
+    padding: '8px 8px',
+  },
+  '& .MuiSelect-icon': {
+    color: '#494949',
+  },
+
+  '& .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#494949',
+  },
+  '&:hover .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#494949',
+  },
+  '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#494949',
+  },
+}));
 
 export const BasicSelect: FC<BasicSelectProps> = ({
   value,
   label = 'Gender',
-  onChange,
   id,
   name,
+  error = false,
+  ...props
 }) => {
   return (
     <Box sx={{ maxWidth: 152 }}>
-      <FormHelperText id={`${id}-label`} sx={{ mb: '5px', color: '#494949' }}>
+      <FormHelperText
+        id={`${id}-label`}
+        sx={{ mb: '5px', color: '#494949' }}
+        error={error}
+      >
         {label}
       </FormHelperText>
 
       <FormControl fullWidth>
-        <Select
+        <StyledSelect
           id={id}
           name={name}
           value={value}
-          onChange={onChange}
           IconComponent={ExpandMoreIcon}
+          error={error}
           variant="outlined"
-          sx={{
-            backgroundColor: 'white',
-            fontSize: '10px',
-            padding: '2px',
-            color: '#5C5C5C',
-            '& .MuiOutlinedInput-notchedOutline': {
-              borderRadius: '8px',
-              borderColor: '#494949',
-            },
-            '& .MuiSelect-select': {
-              padding: '8px 8px',
-            },
-            '& .MuiSelect-icon': {
-              color: '#494949',
-            },
-          }}
+          {...props}
         >
           <MenuItem value="male">Male</MenuItem>
           <MenuItem value="female">Female</MenuItem>
-        </Select>
+        </StyledSelect>
       </FormControl>
     </Box>
   );
