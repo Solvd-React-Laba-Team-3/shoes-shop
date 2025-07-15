79b5efe3758dd21f00645874514567a80d859c3a
Jefferson Lima <Jefferson.fl123@gmail.com>
2025-07-15 12:47:23 -0300

    style: runs prettier on files

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -5,7 +5,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { FC, PropsWithChildren } from 'react';
 
 export const QueryProvider: FC<PropsWithChildren> = ({ children }) => {
-  const queryClient = getQueryClient()
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
