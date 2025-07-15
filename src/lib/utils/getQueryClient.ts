19c5c82421a61a525136b9dd110d132e3f58209b
Jefferson Lima <Jefferson.fl123@gmail.com>
2025-07-15 08:51:15 -0300

    feat: configures react query

---
+++ src/lib/utils/getQueryClient.ts
@@ -0,0 +1,32 @@
+import { QUERIES_STALE_TIME } from '@/constants/queriesStaleTime';
+import {
+  QueryClient,
+  defaultShouldDehydrateQuery,
+  isServer,
+} from '@tanstack/react-query';
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: QUERIES_STALE_TIME,
+      },
+      dehydrate: {
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === 'pending',
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+export function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
