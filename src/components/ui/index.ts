d80f161945421aec8565166caeb1d911431f0c15
Evgenij <e.ilyin.work@gmail.com>
2025-07-15 17:34:29 +0200

    feat: add accordion component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { Button } from './Button/Button';
 export { IconButton } from './IconButton/IconButton';
 export { Checkbox } from './Checkbox/Checkbox';
+export { Accordion } from './Accordion/Accordion';
