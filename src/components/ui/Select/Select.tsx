055dec454e922bdf95442855305925310dc52a7a
olha36 <olhakucheruk36@gmail.com>
2025-07-15 15:14:31 +0300

    finish styling Select element

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -1,40 +1,62 @@
 'use client';
-import * as React from 'react';
+import { FC } from 'react';
 import Box from '@mui/material/Box';
-import FormLabel from '@mui/material/InputLabel';
+import FormHelperText from '@mui/material/FormHelperText';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 type BasicSelectProps = {
-  gender: string;
+  value: string;
+  label?: string;
+  onChange?: (event: SelectChangeEvent) => void;
+  id?: string;
+  name?: string;
 };
 
-export default function BasicSelect({ gender }: BasicSelectProps) {
+export const BasicSelect: FC<BasicSelectProps> = ({
+  value,
+  label = 'Gender',
+  onChange,
+  id,
+  name,
+}) => {
   return (
-    <Box sx={{ maxWidth: 210 }}>
+    <Box sx={{ maxWidth: 152 }}>
+      <FormHelperText id={`${id}-label`} sx={{ mb: '5px', color: '#494949' }}>
+        {label}
+      </FormHelperText>
+
       <FormControl fullWidth>
-        <FormLabel id="demo-simple-select-label">{gender}</FormLabel>
         <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          label="Age"
+          id={id}
+          name={name}
+          value={value}
+          onChange={onChange}
           IconComponent={ExpandMoreIcon}
           variant="outlined"
           sx={{
+            backgroundColor: 'white',
+            fontSize: '10px',
+            padding: '2px',
+            color: '#5C5C5C',
             '& .MuiOutlinedInput-notchedOutline': {
               borderRadius: '8px',
               borderColor: '#494949',
             },
-
-            backgroundColor: 'white',
+            '& .MuiSelect-select': {
+              padding: '8px 8px',
+            },
+            '& .MuiSelect-icon': {
+              color: '#494949',
+            },
           }}
         >
-          <MenuItem value={'male'}>Male</MenuItem>
-          <MenuItem value={'female'}>Female</MenuItem>
+          <MenuItem value="male">Male</MenuItem>
+          <MenuItem value="female">Female</MenuItem>
         </Select>
       </FormControl>
     </Box>
   );
-}
+};
