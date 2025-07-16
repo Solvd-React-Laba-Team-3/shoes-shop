d367e80416bfbb0505c02d5ffd963c545b7dde99
olha36 <olhakucheruk36@gmail.com>
2025-07-16 13:42:30 +0300

    fix(BasicSelect): add required options prop to tests + make BasicSelect component reusable by accepting options array

--- src/components/ui/Select/select.test.tsx
+++ src/components/ui/Select/select.test.tsx
@@ -1,21 +1,28 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { BasicSelect } from './Select';
 
+const options = [
+  { value: 'male', label: 'Male' },
+  { value: 'female', label: 'Female' },
+];
+
 describe('DasicSelect', () => {
   test('renders with correct selected value', () => {
-    render(<BasicSelect value="male" label="gender" />);
+    render(<BasicSelect value="male" label="gender" options={options} />);
 
     expect(screen.getByText('Male')).toBeInTheDocument();
   });
 
   test('renders label correctly', () => {
-    render(<BasicSelect value="female" label="Gender" />);
+    render(<BasicSelect value="female" label="Gender" options={options} />);
 
     expect(screen.getByText('Gender')).toBeInTheDocument();
   });
 
   test('renders all options when select is open', () => {
-    render(<BasicSelect value="" label="Gender" id="gender" displayEmpty />);
+    render(
+      <BasicSelect value="" label="Gender" id="gender" options={options} />
+    );
 
     const label = screen.getByText('Gender');
     const select = label.parentElement?.querySelector('[role="combobox"]');
