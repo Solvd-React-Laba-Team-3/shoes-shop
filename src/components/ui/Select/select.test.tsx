ae630893bbf697d6be3777df850ebf12c005b7d5
olha36 <olhakucheruk36@gmail.com>
2025-07-16 11:28:12 +0300

    write a test that checks whether select renders all options when select is open

--- src/components/ui/Select/select.test.tsx
+++ src/components/ui/Select/select.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BasicSelect } from './Select';
 
 describe('DasicSelect', () => {
@@ -12,5 +12,19 @@ describe('DasicSelect', () => {
     render(<BasicSelect value="female" label="Gender" />);
 
     expect(screen.getByText('Gender')).toBeInTheDocument();
+  });
+
+  test('renders all options when select is open', () => {
+    render(<BasicSelect value="" label="Gender" id="gender" displayEmpty />);
+
+    const label = screen.getByText('Gender');
+    const select = label.parentElement?.querySelector('[role="combobox"]');
+
+    expect(select).toBeTruthy();
+
+    fireEvent.mouseDown(select as Element);
+
+    expect(screen.getByText('Male')).toBeInTheDocument();
+    expect(screen.getByText('Female')).toBeInTheDocument();
   });
 });
