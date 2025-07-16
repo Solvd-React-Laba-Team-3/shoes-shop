430b45d503ae2d43e10e03c186bfa81d00722066
DiegoJabie142 <diegojabie@gmail.com>
2025-07-16 08:01:40 -0300

    Little fix in index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,7 @@
 export { Button } from './Button/Button';
 export { IconButton } from './IconButton/IconButton';
 export { ToggleButton } from './ToggleButton/ToggleButton';
+export { Checkbox } from './Checkbox/Checkbox';
+export { Accordion } from './Accordion/Accordion';
 export { PopUpMenu } from './PopUpMenu/PopUpMenu';
 export { MenuItem } from './MenuItem/MenuItem';
