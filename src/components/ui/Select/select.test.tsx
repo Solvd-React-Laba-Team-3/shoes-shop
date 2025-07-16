46d62e6802f0e79c2d87d51e14ce682d021ab412
olha36 <olhakucheruk36@gmail.com>
2025-07-16 10:58:51 +0300

    write a unit test for checking if select component renders with correct selected value

---
+++ src/components/ui/Select/select.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { BasicSelect } from './Select';
+
+describe('DasicSelect', () => {
+  test('renders with correct selected value', () => {
+    render(<BasicSelect value="male" label="gender" />);
+
+    expect(screen.getByText('Male')).toBeInTheDocument();
+  });
+});
