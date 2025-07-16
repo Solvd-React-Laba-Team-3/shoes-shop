cfa8bc6643f23ea4d1df569a2e2f0d4d5f23fe24
Jefferson Lima <Jefferson.fl123@gmail.com>
2025-07-16 16:38:00 -0300

    style: code review changes

--- src/types/strapiQueryParams.ts
+++ src/types/strapiQueryParams.ts
@@ -1,9 +1,13 @@
+type SortOption<Fields extends string> =
+  | `${Fields}:${'asc' | 'desc'}`
+  | Array<`${Fields}:${'asc' | 'desc'}`>;
+
 export type StrapiQueryParams<Fields extends string> = {
   pagination?: {
     page?: number;
     pageSize?: number;
   };
-  sort?: `${Fields}:${'asc' | 'desc'}` | Array<`${Fields}:${'asc' | 'desc'}`>;
+  sort?: SortOption<Fields>;
   populate?: '*' | Fields | Fields[];
   filters?: Partial<Record<Fields, string | number | boolean | string[]>>;
 };
