d294721daa23fdcb96a3a3528125b2bfb6523a5b
EcchiGrill <dmitry.oborsky@gmail.com>
2025-07-18 22:09:14 +0300

    :bug: Fixed getSizesOptions

--- src/api/size/getSizesOptions.ts
+++ src/api/size/getSizesOptions.ts
@@ -4,17 +4,15 @@ import { StrapiPaginatedData } from '@/types/api/StrapiPaginatedData';
 import { StrapiQueryParams } from '@/types/api/StrapiQueryParams';
 import { queryOptions } from '@tanstack/react-query';
 
-const GET_SIZES_QUERY_KEY = 'sizes';
+export const GET_SIZES_QUERY_KEY = 'sizes';
 
 export const getSizesOptions = (queryParams?: StrapiQueryParams<keyof Size>) =>
   queryOptions({
     queryKey: [GET_SIZES_QUERY_KEY, queryParams],
-    queryFn: async () => {
-      const response = await fetchApi<StrapiPaginatedData<Size>>({
+    queryFn: async () =>
+      await fetchApi<StrapiPaginatedData<Size>>({
         endpoint: '/sizes',
         method: 'GET',
-        body: queryParams,
-      });
-      return response;
-    },
+        queryParams,
+      }),
   });
