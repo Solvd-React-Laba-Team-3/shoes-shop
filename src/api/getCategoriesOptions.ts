6c27c7c67de16a972e48b79dda87390f15494daf
DiegoJabie142 <diegojabie@gmail.com>
2025-07-18 15:40:05 -0300

    Add Category type and fetch categories query options

---
+++ src/api/getCategoriesOptions.ts
@@ -0,0 +1,23 @@
+import { fetchApi } from '@/lib/utils';
+import { Category } from '@/types/Category';
+import { StrapiPaginatedData } from '@/types/api/StrapiPaginatedData';
+import { StrapiQueryParams } from '@/types/api/StrapiQueryParams';
+import { queryOptions } from '@tanstack/react-query';
+
+export const GET_CATEGORIES_QUERY_KEY = 'categories';
+
+export const getCategoriesOptions = (
+  queryParams?: StrapiQueryParams<keyof Category>
+) =>
+  queryOptions({
+    queryKey: [GET_CATEGORIES_QUERY_KEY, queryParams],
+    queryFn: async () => {
+      const response = await fetchApi<StrapiPaginatedData<Category>>({
+        endpoint: '/categories',
+        method: 'GET',
+        body: queryParams,
+      });
+
+      return response;
+    },
+  });
