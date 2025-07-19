6b2ba6724a85c234f9bce9866560647d059ebdb0
olha36 <olhakucheruk36@gmail.com>
2025-07-19 09:57:49 +0300

    move test file for sign up and clean comments

--- src/app/auth/signup/sign-up.test.tsx
+++ src/app/auth/signup/sign-up.test.tsx
@@ -9,18 +9,15 @@ describe('SignUp', () => {
       screen.getByRole('heading', { name: /create an account/i })
     ).toBeInTheDocument();
 
-    // Input fields
     expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/email/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/^Password/i)).toBeInTheDocument(); // "Password"
     expect(screen.getByLabelText(/Confirm password/i)).toBeInTheDocument();
 
-    // Button
     expect(
       screen.getByRole('button', { name: /sign up/i })
     ).toBeInTheDocument();
 
-    // Footer
     expect(screen.getByText(/already have an account/i)).toBeInTheDocument();
     expect(screen.getByRole('link', { name: /log in/i })).toBeInTheDocument();
   });
