cdc72a2407f53d27c7e3f97ace254b5f9930f525
Evgenij <e.ilyin.work@gmail.com>
2025-07-24 21:15:20 +0200

    update: added prop, changes page size

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,12 +1,13 @@
-import { Grid } from '@mui/material';
 import { ProductCard } from '../ProductCard';
 import { Product } from '@/types/Product';
+import { Grid } from '@mui/material';
+import { FC } from 'react';
 
 type ProductListProps = {
   products: Product[];
 };
 
-export function ProductList({ products }: ProductListProps) {
+export const ProductList: FC<ProductListProps> = ({ products }) => {
   return (
     <Grid
       container
@@ -14,10 +15,10 @@ export function ProductList({ products }: ProductListProps) {
       rowSpacing={{ xs: 2, md: 5 }}
     >
       {products.map((product) => (
-        <Grid key={product.id} size={{ xs: 6, sm: 4, md: 3 }}>
+        <Grid key={product.id} size={{ xs: 6, sm: 4, md: 3, lg: 2 }}>
           <ProductCard {...product} cardType="actionMenu" />
         </Grid>
       ))}
     </Grid>
   );
-}
+};
