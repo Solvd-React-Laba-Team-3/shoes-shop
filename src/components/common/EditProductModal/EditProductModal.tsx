e227a3543c8b5e83f994b560009c49da459bfbeb
olha36 <olhakucheruk36@gmail.com>
2025-07-31 15:05:24 +0300

    move files

--- src/components/common/EditProductModal/EditProductModal.tsx
+++ src/components/common/EditProductModal/EditProductModal.tsx
@@ -1,34 +1,33 @@
 'use client';
 
 import { Product } from '@/components/ui/ProductPage/Product';
-import type { ProductData } from '@/components/ui/ProductPage/Product';
+import type { ProductData } from '@/components/ui/ProductPage/productForm.schema';
 import Dialog from '@mui/material/Dialog';
 import React from 'react';
 
-export default function EditPage() {
-  const [open, setOpen] = React.useState(false);
+interface EditPageProps {
+  open: boolean;
+  onClose: () => void;
+  onSubmit: (data: ProductData) => void;
+}
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleEdit = (data: ProductData) => {
-    console.log('Edit submitted:', data);
-  };
-
+export const EditProductModal = ({
+  open,
+  onClose,
+  onSubmit,
+}: EditPageProps) => {
   return (
     <>
       <Dialog
         open={open}
-        onClose={handleClose}
+        onClose={onClose}
         aria-labelledby="draggable-dialog-title"
         fullWidth
         maxWidth={false}
-        slotProps={{
-          paper: {
-            sx: {
-              padding: '57px 0 40px 57px',
-            },
+        sx={{
+          '& .MuiDialog-paper': {
+            maxWidth: '1487px',
+            padding: '57px 0 40px 57px',
           },
         }}
       >
@@ -43,9 +42,9 @@ export default function EditPage() {
               "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with",
             size: [],
           }}
-          onSubmit={handleEdit}
+          onSubmit={onSubmit}
         />
       </Dialog>
     </>
   );
-}
+};
