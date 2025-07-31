7c6db58bbc7b0b46b9c2a6a23fd1bacf5b1d12d1
Jefferson Lima <Jefferson.fl123@gmail.com>
2025-07-31 13:07:41 -0300

    feat: creates FileDropZoneComponent

---
+++ src/components/FileDropZone/FileDropZone.tsx
@@ -0,0 +1,68 @@
+import { Box, styled, Typography } from '@mui/material';
+import InsertPhotoOutlined from '@mui/icons-material/InsertPhotoOutlined';
+import React, { useCallback } from 'react';
+
+type FileDropZoneProps = {
+  onFilesDropped: (files: File[]) => void;
+};
+
+const StyledDropZone = styled(Box)(({ theme }) => ({
+  border: `1px dashed ${theme.palette.text.secondary}`,
+  borderRadius: `${theme.shape.borderRadius}`,
+  display: 'flex',
+  alignItems: 'center',
+  width: '320px',
+  height: '305px',
+  justifyContent: 'center',
+  cursor: 'pointer',
+  padding: '20px',
+}));
+
+export const FileDropZone: React.FC<FileDropZoneProps> = ({
+  onFilesDropped,
+}) => {
+  const fileInputRef = React.useRef<HTMLInputElement | null>(null);
+  const handleDrop = useCallback(
+    (event: React.DragEvent<HTMLDivElement>) => {
+      event.preventDefault();
+      const droppedFiles = Array.from(event.dataTransfer.files);
+      onFilesDropped(droppedFiles);
+    },
+    [onFilesDropped]
+  );
+
+  return (
+    <>
+      <StyledDropZone
+        onDrop={handleDrop}
+        onDragOver={(e) => e.preventDefault()}
+        onClick={() => fileInputRef.current?.click()}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '10px',
+            width: '172px',
+          }}
+        >
+          <InsertPhotoOutlined fontSize="large" color="inherit" />
+          <Typography
+            sx={{ lineHeight: '100%' }}
+            variant="caption"
+            color="text.secondary"
+          >
+            Drop your image here, or click to select a file
+          </Typography>
+        </Box>
+      </StyledDropZone>
+      <input
+        ref={fileInputRef}
+        type="file"
+        style={{ display: 'none' }}
+        name="file"
+      />
+    </>
+  );
+};
