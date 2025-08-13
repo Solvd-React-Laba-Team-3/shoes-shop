e074142af8b10612a218a0103e33e463f49e8d22
Jefferson Lima <Jefferson.fl123@gmail.com>
2025-08-13 16:59:59 -0300

    test: fix tests

--- src/components/common/ProductListFallback/productListFallback.test.tsx
+++ src/components/common/ProductListFallback/productListFallback.test.tsx
@@ -3,10 +3,9 @@ import { ProductListFallback } from './ProductListFallback';
 import '@testing-library/jest-dom';
 
 describe('ProductListFallback', () => {
-
   it('renders 12 product skeleton groups (each with 4 skeletons)', () => {
     const { container } = render(<ProductListFallback />);
     const skeletons = container.querySelectorAll('.MuiSkeleton-root');
-    expect(skeletons.length).toBe(51);
+    expect(skeletons.length).toBe(53);
   });
 });
