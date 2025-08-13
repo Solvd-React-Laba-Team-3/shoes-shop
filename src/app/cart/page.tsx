713583c4cae8068f5c2c1c16d4b40d78c3577172
olha36 <olhakucheruk36@gmail.com>
2025-08-13 13:55:28 +0300

    Made an item image with fixed aspect ratio 1/1, set min-width: max-content, added  border bottom to the item container as in design, made padding 20px starting from 1200 and less

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -17,22 +17,19 @@ export default function Cart() {
         <CartFallback />
       ) : (
         <Box
-          sx={{
+          sx={(theme) => ({
             display: 'flex',
             flexDirection: { xs: 'column', md: 'row' },
             justifyContent: { md: 'space-between' },
             alignItems: { xs: 'stretch', md: 'flex-start' },
             gap: { xs: 3, sm: 4, md: 6, lg: 8 },
-            padding: {
-              xs: '8px',
-              sm: '16px',
-              md: '48px 64px',
-              lg: '64px 120px',
-              xl: '80px 160px',
+            padding: '80px 160px',
+            [theme.breakpoints.down('lg')]: {
+              padding: '20px', // applies for screen widths <= 1200px
             },
             maxWidth: '1600px',
             margin: '0 auto',
-          }}
+          })}
         >
           <Stack sx={{ flex: 2, width: '100%' }}>
             <Typography
