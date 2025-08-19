35a881afa8643b30f48ac5a84d14434e717deaf7
Evgenij <e.ilyin.work@gmail.com>
2025-08-20 01:29:33 +0200

    update: move sx to styled

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,11 +3,49 @@
 import { Header } from '@/components/common/Header';
 import { Button } from '@/components/ui';
 import { Box, Stack, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
+const StyledWrapper = styled(Box)(({ theme }) => ({
+  flex: 1,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  position: 'absolute',
+  height: '100%',
+  padding: '0 16px',
+
+  [theme.breakpoints.up('sm')]: { padding: '0 5%' },
+  [theme.breakpoints.up('md')]: { padding: '0 10%' },
+  [theme.breakpoints.up('lg')]: {
+    position: 'relative',
+    padding: 0,
+  },
+}));
+
+const StyledTextContainer = styled(Stack)(({ theme }) => ({
+  width: '100%',
+  padding: '24px 16px',
+  gap: 20,
+  justifyContent: 'center',
+  backgroundColor: 'rgba(242,242,242, 0.5)',
+  backdropFilter: 'blur(10px)',
+  borderRadius: 10,
+
+  [theme.breakpoints.up('sm')]: { padding: '32px 24px' },
+  [theme.breakpoints.up('md')]: { padding: '36px 28px' },
+  [theme.breakpoints.up('lg')]: {
+    width: '56%',
+    padding: 0,
+    backgroundColor: 'transparent',
+    backdropFilter: 'none',
+  },
+}));
+
 export default function NotFound() {
   const router = useRouter();
+
   return (
     <Box
       sx={{
@@ -26,48 +64,22 @@ export default function NotFound() {
           flex: '1 1 auto',
         }}
       >
-        <Box
-          sx={{
-            flex: 1,
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            position: { xs: 'absolute', lg: 'relative' },
-            height: '100%',
-            padding: { xs: '0 16px', sm: '0 5%', md: '0 10%', lg: 0 },
-          }}
-        >
-          <Stack
-            direction={'column'}
-            sx={{
-              width: { xs: '100%', lg: '56%' },
-              padding: {
-                xs: '24px 16px',
-                sm: '32px 24px',
-                md: '36px 28px',
-                lg: 0,
-              },
-              gap: '20px',
-              justifyContent: 'center',
-              backgroundColor: {
-                xs: 'rgba(242,242,242, 0.5)',
-                lg: 'transparent',
-              },
-              backdropFilter: { xs: 'blur(10px)', lg: 0 },
-              borderRadius: '10px',
-            }}
-          >
-            <Typography variant="h2" component={'h1'}>
+        <StyledWrapper>
+          <StyledTextContainer direction="column">
+            <Typography variant="h2" component="h1">
               Error 404
             </Typography>
-            <Typography variant="h6" component={'p'} color="text.secondary">
+
+            <Typography variant="h6" component="p" color="text.secondary">
               Looks like you&apos;ve wandered off the map. The page you&apos;re
               looking for doesn&apos;t exist, was moved, or might have been
               retired.
             </Typography>
-            <Typography variant="h6" component={'p'} color="text.secondary">
+
+            <Typography variant="h6" component="p" color="text.secondary">
               Don&apos;t worry - use the navigation below
             </Typography>
+
             <Stack
               sx={{
                 flexDirection: { xs: 'column', sm: 'row' },
@@ -83,6 +95,7 @@ export default function NotFound() {
               >
                 Go back
               </Button>
+
               <Button
                 sx={{ width: { xs: '100%', sm: '152px' }, height: '40px' }}
                 variant="contained"
@@ -91,23 +104,16 @@ export default function NotFound() {
                 Home
               </Button>
             </Stack>
-          </Stack>
-        </Box>
-        <Box
-          sx={{
-            flex: 1,
-          }}
-        >
+          </StyledTextContainer>
+        </StyledWrapper>
+
+        <Box sx={{ flex: 1 }}>
           <Image
-            src={'/page-404-image.png'}
+            src="/page-404-image.png"
             width={1000}
             height={1000}
             alt="page 404 - image"
-            style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-            }}
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           />
         </Box>
       </Box>
