666e4b4878178cdd6386ecfa8ea3318287d16e45
olha36 <olhakucheruk36@gmail.com>
2025-08-20 15:35:48 +0300

    fix sign up page

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import {
+  Button,
   FormErrorMessage,
   LabeledTextfield,
   Link,
   ReviewPanel,
 } from '@/components/ui';
-import { Box, Typography } from '@mui/material';
+import { Box, Divider, Typography } from '@mui/material';
 import Image from 'next/image';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -16,6 +17,9 @@ import { SignUpSchema, signUpSchema } from './sign-up.schema';
 import { useRouter } from 'next/navigation';
 import { LoaderButton } from '@/components/LoaderButton';
 import registerImage from '../../../../public/register.jpg';
+import { useState } from 'react';
+import { Loading } from '@/components/LoadingProgress';
+import { signIn } from 'next-auth/react';
 
 export default function SignUp() {
   const router = useRouter();
@@ -36,6 +40,7 @@ export default function SignUp() {
   });
 
   const { mutate: registerUser, error, isPending } = useRegister();
+  const [isGoogleLoading, setIsGoogleLoading] = useState<boolean>(false);
 
   const onSubmit = (data: SignUpSchema) => {
     const { name, email, password } = data;
@@ -49,6 +54,65 @@ export default function SignUp() {
       }
     );
   };
+
+  const loginWithGoogle = async () => {
+    setIsGoogleLoading(true);
+    try {
+      await signIn('google', { callbackUrl: '/profile/products' });
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsGoogleLoading(false);
+    }
+  };
+  // const loginWithGoogle = async () => {
+  //   try {
+  //     setIsGoogleLoading(true);
+
+  //     // 1. Sign in with Google
+  //     await signIn('google', { callbackUrl: '/profile/products' });
+
+  //     // if (!res?.ok) throw new Error('Google login failed');
+
+  //     // 2. Get session info from NextAuth
+  //     const session = await getSession();
+  //     const user = session?.user;
+  //     if (!user || !user.email)
+  //       throw new Error('No user returned from Google login');
+
+  //     // 3. Prepare a name to send to Strapi
+  //     const nameToSend = user.name || user.username || 'User';
+
+  //     // 4. Send Google info to Strapi and get Strapi JWT
+  //     const strapiRes = await fetch(
+  //       `${process.env.NEXT_PUBLIC_API_URL}/auth/google-custom`,
+  //       {
+  //         method: 'POST',
+  //         headers: { 'Content-Type': 'application/json' },
+  //         body: JSON.stringify({
+  //           email: user.email,
+  //           name: nameToSend,
+  //         }),
+  //       }
+  //     );
+
+  //     const data = await strapiRes.json();
+  //     if (!strapiRes.ok)
+  //       throw new Error(data.error || 'Strapi Google login failed');
+
+  //     // 5. Store Strapi JWT locally
+  //     localStorage.setItem('strapiToken', data.jwt);
+
+  //     console.log('Strapi JWT stored:', data.jwt);
+
+  //     // 6. Redirect to profile page
+  //     window.location.href = '/profile/products';
+  //   } catch (err) {
+  //     console.error(err);
+  //   } finally {
+  //     setIsGoogleLoading(false);
+  //   }
+  // };
 
   return (
     <>
@@ -134,6 +198,52 @@ export default function SignUp() {
             text="Sign up"
             loadingText="Submitting..."
           />
+
+          <Divider
+            sx={{
+              width: '100%',
+              height: 'auto',
+              color: 'black',
+            }}
+          >
+            or sign up with
+          </Divider>
+
+          <Button
+            disabled={isGoogleLoading}
+            onClick={loginWithGoogle}
+            variant="outlined"
+            size="large"
+          >
+            {isGoogleLoading && <Loading />}
+            <svg
+              style={{ marginRight: '8px' }}
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="20"
+              height="20"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#FFC107"
+                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+              <path
+                fill="#FF3D00"
+                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+              ></path>
+              <path
+                fill="#4CAF50"
+                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+              ></path>
+              <path
+                fill="#1976D2"
+                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+              ></path>
+            </svg>
+            Google
+          </Button>
         </Box>
       </AuthContainer>
 
