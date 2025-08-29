5d50233f511162cb8331bec95deeeb0a4860d75c
Jefferson Lima <Jefferson.fl123@gmail.com>
2025-08-29 14:34:40 -0300

    feat: changes image upload strategy

--- src/api/uploadFile/useUploadFile.tsx
+++ src/api/uploadFile/useUploadFile.tsx
@@ -6,6 +6,26 @@ const uploadFile = async (files: File | File[]): Promise<StrapiFile[]> => {
   const formData = new FormData();
   const filesToUpload = Array.isArray(files) ? files : [files];
 
+  if (filesToUpload.length > 5) {
+    const uploadPromises = [];
+    for (let i = 0; i < filesToUpload.length; i += 5) {
+      const chunk = filesToUpload.slice(i, i + 5);
+      const chunkFormData = new FormData();
+      chunk.forEach((file) => {
+        chunkFormData.append('files', file);
+      });
+      uploadPromises.push(
+        fetchApi<StrapiFile[]>({
+          endpoint: `/upload`,
+          method: 'POST',
+          body: chunkFormData,
+        })
+      );
+    }
+    const responses = await Promise.all(uploadPromises);
+    return responses.flat();
+  }
+
   filesToUpload.forEach((file) => {
     formData.append('files', file);
   });
