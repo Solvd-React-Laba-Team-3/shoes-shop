645a6d36ba5c7f86d0ff671c84b74db2ea27be17
Jefferson Lima <Jefferson.fl123@gmail.com>
2025-08-29 17:25:04 -0300

    style: code style fixes

--- src/api/uploadFile/useUploadFile.tsx
+++ src/api/uploadFile/useUploadFile.tsx
@@ -1,3 +1,4 @@
+import { PRODUCT_IMAGES_LIMIT } from '@/constants/productImagesLimit';
 import { fetchApi } from '@/lib/utils';
 import { File as StrapiFile } from '@/types/api/File';
 import { useMutation } from '@tanstack/react-query';
@@ -6,10 +7,10 @@ const uploadFile = async (files: File | File[]): Promise<StrapiFile[]> => {
   const formData = new FormData();
   const filesToUpload = Array.isArray(files) ? files : [files];
 
-  if (filesToUpload.length > 5) {
+  if (filesToUpload.length > PRODUCT_IMAGES_LIMIT) {
     const uploadPromises = [];
-    for (let i = 0; i < filesToUpload.length; i += 5) {
-      const chunk = filesToUpload.slice(i, i + 5);
+    for (let i = 0; i < filesToUpload.length; i += PRODUCT_IMAGES_LIMIT) {
+      const chunk = filesToUpload.slice(i, i + PRODUCT_IMAGES_LIMIT);
       const chunkFormData = new FormData();
       chunk.forEach((file) => {
         chunkFormData.append('files', file);
